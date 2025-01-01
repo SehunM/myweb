8fb4a152f7de1de3f083bb834474c2f5a2ae3da5
Yang Ju Zhi <2271556865@qq.com>
2025-01-02 01:24:50 +0800

    replace version 1

--- docker/run.sh
+++ docker/run.sh
@@ -8,10 +8,10 @@ Project_Web_Src_Package=
 if [ ! -z "./${Project_Web_Src_Package:=MyWebApp.war}" ];then 
     if [ -e "./$Project_Web_Src_Package" -o  ! -e "./$Project_Web_Src_Package" ];then
         rm -rf ./$Project_Web_Src_Package
-        mv ../target/$Project_Web_Src_Package ./ -f
+        \cp ../target/$Project_Web_Src_Package ./ -f
     fi
 fi
 docker rm -f tomcat 2>/dev/null
-docker images | grep 'none' | awk '{system("docker rmi "$3"")}'
-docker build -t tomcat:v1 --no-cache . && \
+docker build -t tomcat:v1 --no-cache .
 docker run --name tomcat --network host --publish 8080:8080 -d tomcat:v1
+docker images | grep 'none' | awk '{system("docker rmi "$3"")}'
